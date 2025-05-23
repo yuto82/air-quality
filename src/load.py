4127e8d332bd5e932ea4dbe09ef999806c78acab
yuto82 <rgritsal@gmail.com>
2025-05-23 19:39:57 +0200

    add: data loading to database

--- src/load.py
+++ src/load.py
@@ -1,9 +1,39 @@
+import sys
 import pandas as pd
 from pathlib import Path
 from settings.config import Config
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,text
 
-engine = create_engine(Config.DATABASE_URL)
+def create_db_engine():
+    try:
+        engine = create_engine(Config.DATABASE_URL)
 
-df = pd.read_csv(Path(__file__).parent / "data" / "data.csv")
-df.to_sql("weather_db", engine, if_exists='replace', index=False)
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+        print(f"Engine created successfully!")
+        
+        return(engine)
+    
+    except Exception as e:
+        print(f"Error creating engine: {e}")
+        sys.exit(1)
+
+def load_db_data(data):
+    engine = create_db_engine()
+
+    try:
+        data.to_sql("weather_data", engine, if_exists='replace', index=False)
+        print(f"Data loaded into the table successfully!")
+
+    except Exception as e:
+        print(f"Error loading data to PostgreSQL: {e}")
+        sys.exit(1)
+
+def main():
+    data_path = Path(__file__).parent / "data" / "data.csv"
+    data = pd.read_csv(data_path)
+
+    load_db_data(data)
+
+if __name__ == "__main__":
+    main()
